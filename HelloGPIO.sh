bf1dec9cedbed38107ad490ef86efd7047964772
gskielian <greg@techlabeducation.com>
2013-07-18 22:21:49 -0700

    Update HelloGPIO.sh

--- HelloGPIO.sh
+++ HelloGPIO.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-while [ 1 ]
-do
 
 echo 11 > /sys/class/gpio/export 
 
 echo out > /sys/class/gpio/gpio11/direction
 
+
+while [ 1 ]
+do
+
 echo 1 > /sys/class/gpio/gpio11/value
 
+sleep 1
+
 echo 0 > /sys/class/gpio/gpio11/value
+
+sleep 3
 
 done
